bf283293beb7ddcc27f8ec57e1a64391372c519b
Nguyen Minh Tuan <minhtuan180305@gmail.com>
2026-05-10 23:15:38 +0700

    Nộp bài đầy đủ: Code + Database + README

---
+++ db.sql
@@ -0,0 +1,64 @@
+/*M!999999\- enable the sandbox mode */ 
+-- MariaDB dump 10.19-11.8.6-MariaDB, for debian-linux-gnu (aarch64)
+--
+-- Host: localhost    Database: socialnet
+-- ------------------------------------------------------
+-- Server version	11.8.6-MariaDB-6 from Debian
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*M!100616 SET @OLD_NOTE_VERBOSITY=@@NOTE_VERBOSITY, NOTE_VERBOSITY=0 */;
+
+--
+-- Table structure for table `account`
+--
+
+DROP TABLE IF EXISTS `account`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `account` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `fullname` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `description` text DEFAULT NULL,
+  `mssv` varchar(20) DEFAULT 'Not set',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `account`
+--
+
+SET @OLD_AUTOCOMMIT=@@AUTOCOMMIT, @@AUTOCOMMIT=0;
+LOCK TABLES `account` WRITE;
+/*!40000 ALTER TABLE `account` DISABLE KEYS */;
+INSERT INTO `account` VALUES
+(1,'tuan','NGUYEN MINH TUAN','$2y$12$oK3CY0SYmVNIvM.VWwKXyO0dHy0Gm7i8JiAq4eCPTN5WKzmWk9.UO','TOI LA TUAN DEP TRAI SO 1 0 2','20239624'),
+(2,'duong','HA THAI DUONG','$2y$12$xT7E.ciGJeBR8Bd.Awh0kOZqiQvQ9auoKr9IRUz/bh4QhsGoiTLWq','TOI LA DUONG DAY!!!!','20239999'),
+(3,'hoa','DO HUU HOA','$2y$12$EHW1YQfkv5cIWnKJprB8z.MQmbcpZPOWEskwpr/ZE4i9bxvfb4iQy','TOI LA HOA HAHAHHAHAHA','20238888'),
+(4,'trung','TRAN THANH TRUNG','$2y$12$RChIzexTJKvKtaNQJTDYYOXZFbs5wLTz/fUfn0qbXM81oCNc4lQFC','TOI LA TRUNG DEN DAY','20238458');
+/*!40000 ALTER TABLE `account` ENABLE KEYS */;
+UNLOCK TABLES;
+COMMIT;
+SET AUTOCOMMIT=@OLD_AUTOCOMMIT;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*M!100616 SET NOTE_VERBOSITY=@OLD_NOTE_VERBOSITY */;
+
+-- Dump completed on 2026-05-10 23:15:31
